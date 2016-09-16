f292b395c8fae85346d3186962d855cd2ea5771d
Vivek Vaddina <vaddinavivek@gmail.com>
2016-09-16 05:18:48 +0200

    code to predict class probabilities on new data

--- eval.py
+++ eval.py
@@ -1,83 +1,100 @@
 #! /usr/bin/env python
 
-import tensorflow as tf
+""" predict the polarity of sentences """
+
+import logging
+logging.basicConfig(filename="eval.log", level=logging.INFO, format='%(levelname)s: %(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S')
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
 import numpy as np
-import os
-import time
-import datetime
-import data_helpers
-from text_cnn import TextCNN
-from tensorflow.contrib import learn
+import tensorflow as tf
 
-# Parameters
-# ==================================================
+from train import Training
 
-# Eval Parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
-tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
-
-# Misc Parameters
-tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
-tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+ALLOW_SOFT_PLACEMENT    = True
+LOG_DEVICE_PLACEMENT    =  False
 
 
-FLAGS = tf.flags.FLAGS
-FLAGS._parse_flags()
-print("\nParameters:")
-for attr, value in sorted(FLAGS.__flags.items()):
-    print("{}={}".format(attr.upper(), value))
-print("")
+class Oracle:
+    """
+        Class to test the results
+        Input: Text file containing one input sentence per line (assumes clean input)
+        Output:
 
-# CHANGE THIS: Load data. Load your own data here
-if FLAGS.eval_train:
-    x_raw, y_test = data_helpers.load_data_and_labels()
-    y_test = np.argmax(y_test, axis=1)
-else:
-    x_raw = ["a masterpiece four years in the making", "everything is off."]
-    y_test = [1, 0]
+    """
 
-# Map data into vocabulary
-vocab_path = os.path.join(FLAGS.checkpoint_dir, "..", "vocab")
-vocab_processor = learn.preprocessing.VocabularyProcessor.restore(vocab_path)
-x_test = np.array(list(vocab_processor.transform(x_raw)))
+    def __init__(self, resurrect_model_checkpoint):
+        self.tr = Training()
+        self.resurrect_model = resurrect_model_checkpoint
 
-print("\nEvaluating...\n")
+    def predict(self, test_file, cleaned=True):
+        #TODO: preprocess the raw sentences (clean(line))
+        if not cleaned:
+            log.warn('currently only works for cleaned sentences...')
+            return
 
-# Evaluation
-# ==================================================
-checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
-graph = tf.Graph()
-with graph.as_default():
-    session_conf = tf.ConfigProto(
-      allow_soft_placement=FLAGS.allow_soft_placement,
-      log_device_placement=FLAGS.log_device_placement)
-    sess = tf.Session(config=session_conf)
-    with sess.as_default():
-        # Load the saved meta graph and restore variables
-        saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
-        saver.restore(sess, checkpoint_file)
+        with open(test_file) as fl:
+            log.debug('reading sentences...')
+            #TODO: read file in batches
+            sentences = np.array([x.lower().strip() for x in fl.readlines()])
 
-        # Get the placeholders from the graph by name
-        input_x = graph.get_operation_by_name("input_x").outputs[0]
-        # input_y = graph.get_operation_by_name("input_y").outputs[0]
-        dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
+        log.debug("transforming the sentences...")
+        matrix = self.tr.fit_transform(sentences)
 
-        # Tensors we want to evaluate
-        predictions = graph.get_operation_by_name("output/predictions").outputs[0]
+        log.debug('building the graph')
+        graph = tf.Graph()
+        with graph.as_default():
+            session_conf = tf.ConfigProto(
+              allow_soft_placement=ALLOW_SOFT_PLACEMENT,
+              log_device_placement=LOG_DEVICE_PLACEMENT)
+            session_conf.gpu_options.allow_growth=True
+            sess = tf.Session(config=session_conf)
+            with sess.as_default():
+                log.debug('started the session...')
+                # Load the saved meta graph and restore variables
+                saver = tf.train.import_meta_graph("{}.meta".format(self.resurrect_model))
+                saver.restore(sess, self.resurrect_model)
 
-        # Generate batches for one epoch
-        batches = data_helpers.batch_iter(list(x_test), FLAGS.batch_size, 1, shuffle=False)
+                log.debug('restored session, inputting x')
+                # Get the placeholders from the graph by name
+                input_x = graph.get_operation_by_name("input_x").outputs[0]
 
-        # Collect the predictions here
-        all_predictions = []
+                log.debug('getting dropout probs...')
+                dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
 
-        for x_test_batch in batches:
-            batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
-            all_predictions = np.concatenate([all_predictions, batch_predictions])
+                log.debug('extracting predictions graph component...')
+                # Tensors we want to evaluate
+                predictions = graph.get_operation_by_name("output/scores").outputs[0]
+                bin_predictions = graph.get_operation_by_name("output/predictions").outputs[0]
 
-# Print accuracy if y_test is defined
-if y_test is not None:
-    correct_predictions = float(sum(all_predictions == y_test))
-    print("Total number of test examples: {}".format(len(y_test)))
-    print("Accuracy: {:g}".format(correct_predictions/float(len(y_test))))
+                log.debug('output class probabilities...')
+                probas = sess.run(predictions, {input_x: matrix, dropout_keep_prob: 1.0})
+
+                # log.debug('output just binary labels (1 or 0)...')
+                # binary = sess.run(bin_predictions, {input_x: matrix, dropout_keep_prob: 1.0})
+
+                del matrix, saver, predictions, bin_predictions, input_x
+                sess.close()
+
+            sess.close()
+
+        del graph
+        return probas
+
+
+if __name__ == '__main__':
+
+    # <Insert checkpoint model here...>
+    # model_path = ''
+    # orkl = Oracle(model_path)
+    #
+    # Input file containing sentences to be classified (one input sentence per line)
+    # test_file = ''
+    #
+    # Predict class probabilities
+    # probas = orkl.predict(test_file)
+    # print (probas)
+
+    pass
